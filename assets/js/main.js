07ea365493d78dc2dc9ec61a95e8d828710c4b7b
hamidsiddiqi <108910473+hamidsiddiqi@users.noreply.github.com>
2022-07-11 22:26:19 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,31 @@ closeMenu.addEventListener('click', ()=>{
 })
 
 /*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav_link')
 
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        const sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
+            document.querySelector('.nav_menu a[href*='+ sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav_menu a[href*='+ sectionId + ']').classList.remove('active')
+        }
+    })
+}
